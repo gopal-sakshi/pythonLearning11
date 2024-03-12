b0d8b9e995c19ab88b4d3c20472fb0659cd808b3
gopal612 <gsakshi@home.com>
2024-03-12 21:48:22 +0530

    dill & pickle

--- 06_spacy_others/filehandler11.py
+++ 06_spacy_others/filehandler11.py
@@ -11,6 +11,17 @@ filehandler23 = open("06_spacy_others/fasttext_data1.txt")
 print(filehandler23.read())
 
 
-dataToDump = { "clubName": "RM", "manager": "ancelotti" }
-outputFileHandler = open("06_spacy_others/filehandlerOp11.bin", "wb")
-pickle.dump(dataToDump, outputFileHandler)
+# #### data dumped ##############################
+# dataToDump = { "clubName": "RM", "manager": "ancelotti" }
+# outputFileHandler = open("06_spacy_others/filehandlerOp11.bin", "wb")
+# pickle.dump(dataToDump, outputFileHandler)
+# #### data dumped ##############################
+
+inputFileHandler = open("06_spacy_others/filehandlerOp11.bin", "rb")
+newdata = pickle.load(inputFileHandler)
+inputFileHandler.close()
+print("newdata =======> ", newdata)
+
+### dumps vs dump
+######## dumps (loads) ---> Instead of taking a file stream object as parameter, it takes a string
+######## dump (load) -----> takes file stream object as parameter;
